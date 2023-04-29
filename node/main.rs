d95d9bacaaf8528216cf433b7608d4a14ac457de
Benjamin Brittain <ben@brittain.org>
2023-04-29 13:57:08 -0400

    Create a executable 'node' that reads a config file

---
+++ node/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use serde::Deserialize;
+use std::path::PathBuf;
+use tokio::{fs::File, io::AsyncReadExt};
+use toml::Table;
+
+#[derive(Parser, Debug)]
+#[command(name = "oryx-node")]
+#[command(author = "Benjamin Brittain. <ben@brittain.org>")]
+#[command(version = "0.1")]
+#[command(about = "Oryx Remote Build Execution node", long_about = None)]
+struct Args {
+    #[arg(long)]
+    config: PathBuf,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    address: std::net::SocketAddr,
+}
+
+/// Read the oryx node config
+async fn read_config(config_file: PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
+    let mut file = File::open(config_file).await?;
+    let mut contents = vec![];
+    file.read_to_end(&mut contents).await?;
+    let contents = std::str::from_utf8(&contents)?;
+    Ok(toml::from_str(contents)?)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let config = read_config(args.config).await?;
+    println!("{config:?}");
+
+    Ok(())
+}
