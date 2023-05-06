d030f7b58b0ca0a0d565422670a68f9ce5c04d64
Benjamin Brittain <ben@brittain.org>
2023-05-05 20:01:43 -0400

    Make a simple blob check test case

--- node/tests/main.rs
+++ node/tests/main.rs
@@ -1,3 +1,6 @@
+use common::Digest;
+use futures::Future;
+use std::str::FromStr;
 use std::sync::Arc;
 use tempfile::NamedTempFile;
 use tokio::net::{UnixListener, UnixStream};
@@ -5,8 +8,12 @@ use tokio_stream::wrappers::UnixListenerStream;
 use tonic::transport::{Channel, Endpoint, Server, Uri};
 use tonic::Request;
 
-#[tokio::test]
-async fn simple_query() {
+async fn oryx_test<F, FRet>(client_test_fut: F)
+where
+    F: FnOnce(Channel) -> FRet,
+    FRet: Future<Output = ()>,
+{
+    // Create a new UDS file
     let socket = NamedTempFile::new().unwrap();
     let socket = Arc::new(socket.into_temp_path());
     std::fs::remove_file(&*socket).unwrap();
@@ -14,6 +21,7 @@ async fn simple_query() {
     let uds = UnixListener::bind(&*socket).unwrap();
     let stream = UnixListenerStream::new(uds);
 
+    // Create a new oryx instance
     let server_fut = async {
         let result = node_lib::start_oryx(
             String::from(""),
@@ -25,6 +33,7 @@ async fn simple_query() {
         assert!(result.is_ok());
     };
 
+    // Create a UDS connection to the oryx instance
     let socket = Arc::clone(&socket);
     let channel = Endpoint::try_from("http://oryx.build")
         .unwrap()
@@ -35,22 +44,31 @@ async fn simple_query() {
         .await
         .unwrap();
 
-    let mut client = protos::ContentAddressableStorageClient::new(channel);
+    // Run the client future
+    let client_fut = async move { client_test_fut(channel).await };
 
-    let client_fut = async {
+    // Run both futures to completion
+    tokio::select! {
+        _ = server_fut => panic!("Server ended execution before client."),
+        _ = client_fut => (),
+    }
+}
+
+#[tokio::test]
+async fn simple_check_blob_exists() {
+    let missing_digest: protos::re::Digest = Digest::from_str("aaaa:5").unwrap().into();
+
+    oryx_test(|channel| async move {
+        let mut client = protos::ContentAddressableStorageClient::new(channel);
         let response = client
             .find_missing_blobs(Request::new(protos::re::FindMissingBlobsRequest {
-                blob_digests: vec![],
+                blob_digests: vec![missing_digest.clone()],
                 instance_name: "".to_string(),
             }))
             .await
             .unwrap()
             .into_inner();
-        assert_eq!(response.missing_blob_digests, vec![]);
-    };
-
-    tokio::select! {
-        _ = server_fut => panic!("Server ended execution before client."),
-        _ = client_fut => (),
-    }
+        assert_eq!(response.missing_blob_digests, vec![missing_digest]);
+    })
+    .await;
 }
